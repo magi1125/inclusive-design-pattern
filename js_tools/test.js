195d27fcb08bf7a5d219875f38a538f82b135350
ota <ota@ota.local>
2017-12-27 19:07:03 +0900

    訳注ツール途中

--- js_tools/test.js
+++ js_tools/test.js
@@ -1,27 +1,55 @@
 const fs = require('fs');
 const jsdom = require('jsdom');
-const { JSDOM } = jsdom;
+const { JSDOM } = jsdom; // JSDOM = jsdom.JSDOM;
 const COMMENT_NODE = 8; // nodeType
+const ELEMENT_NODE = 1; // nodeType
+const COMMENT_PREFIX = 'translator_notes:';
 
-const htmlfiledir = '../source/'
+const source_note_dir = '../translator_notes/';
+const source_html_dir = '../source/';
+const dest_dir = './temp/';
 
-fs.readdir(htmlfiledir, function(err, files){
-    if (err) throw err;
-    const htmlfiles = files.filter(file => { return /.*\.html$/.test(file);});
-    htmlfiles.forEach(file => {
-        JSDOM.fromFile(htmlfiledir + file).then(dom => {
-            let document = dom.window.document;
-            let mainDiv = document.body.childNodes[0];
-            let mainChildren = mainDiv.childNodes;
-        
-            for(let i =0; i < mainChildren.length; i++){
-                let node = mainChildren[i];
-                if(node.nodeType !== COMMENT_NODE) continue;
-                console.log(node.data);
-//                node.parentNode.replaceChild(document.createElement("br"), node);
-            }
-//            console.log(dom.serialize());
-        });
+
+// Load note data
+const text_files = fs.readdirSync(source_note_dir).filter(file => { return /\.txt$/.test(file);});
+text_files.forEach(file => {
+    const source_file_path = source_note_dir + file;
+    const text = fs.readFileSync(source_file_path, 'utf8');
+    const lines = text.split("\n");
+    lines.forEach((line, index) =>{
+        console.log(`${index}: ${line}`);
     });
 });
-console.log("done");
+
+// Process
+const html_files = fs.readdirSync(source_html_dir).filter(file => { return /\.html$/.test(file);});
+html_files.forEach(file => {
+    const source_file_path = source_html_dir + file;
+    const dest_file_path = dest_dir + file;
+    JSDOM.fromFile(source_file_path).then(dom => {
+        console.log(file);
+        const document = dom.window.document;
+        const mainDiv = document.body.childNodes[0];
+        const mainChildren = mainDiv.childNodes;
+        
+        // 訳注の見出しレベルをいい感じにするために現在の見出しレベルを把握
+        let current_heading_level = 0;
+        for(let i =0; i < mainChildren.length; i++){
+            const node = mainChildren[i];
+            if(node.nodeType === ELEMENT_NODE){
+                const element_name = node.nodeName;
+                if(/^[Hh][1-6]$/.test(element_name)){
+                    current_heading_level = Number(element_name.substring(1,2));
+                }
+            } else if(node.nodeType === COMMENT_NODE){
+                const comment_data = node.data.trim();
+                if(comment_data.indexOf(COMMENT_PREFIX) < 0) continue;
+                const comment_id = comment_data.replace(COMMENT_PREFIX, '').trim();
+                console.log(`[Debug]: ${comment_id} / heading:${current_heading_level}`);
+            }
+        }
+        fs.writeFileSync(dest_file_path, dom.serialize());
+    });
+});
+
+
